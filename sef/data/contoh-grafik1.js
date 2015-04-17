65cf110c49ac2774072e2a30a735d31bba921030
emfatoni <em.fatoni@gmail.com>
2015-04-17 15:18:56 +0700

    masukin mockup awal

---
+++ sef/data/contoh-grafik1.js
@@ -0,0 +1,13 @@
+var data1 = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
